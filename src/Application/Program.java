9a58a84056955cfaff6b40221438d810cf835ad7
David-Simas <thecodesimas@gmail.com>
2025-10-05 21:52:27 -0300

    Classe de entidade Departamento

---
+++ src/Application/Program.java
@@ -0,0 +1,17 @@
+package Application;
+
+import model.entities.Department;
+
+/**
+ *
+ * @author David
+ */
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Livros");
+        System.out.println(obj);
+    }
+    
+}
