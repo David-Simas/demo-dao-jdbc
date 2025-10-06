552ec4b2e4d88801a3182d5d7332e82a1a4f8a30
David-Simas <thecodesimas@gmail.com>
2025-10-05 22:00:45 -0300

    Classe de entidade Vendedor

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -11,7 +14,9 @@ public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Livros");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        System.out.println(seller);
     }
     
 }
