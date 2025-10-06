96e6d9266ad8b4b53675f097a485e7d097c782c6
David-Simas <thecodesimas@gmail.com>
2025-10-06 08:47:19 -0300

    SellerDaoJDBC e DaoFactory

---
+++ src/model/dao/implementation/SellerDaoJDBC.java
@@ -0,0 +1,38 @@
+package model.dao.implementation;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+/**
+ *
+ * @author David
+ */
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void insert(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(Seller obj) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
